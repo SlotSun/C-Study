499a417b81fc4b9c46e45e785ad9e90ad325850a
Slot <1059819898@qq.com>
2020-05-02 01:57:17 +0800

    ===

--- hFile/leetCode.h
+++ hFile/leetCode.h
@@ -74,6 +74,25 @@ LinkList addTwoNumbers(LNode *l1, LNode *l2) {
     }
     return l3;
 }
+//无重复字符的最长子串
+int lengthOfLongestSubstring(char * s){
+    int start = 0, end = 0, maxlen = 0;
+    char map[256] = {0};
+    map[(int)*(s+start)] = 1;
+    while( *(s+end) != 0 )
+    {
+        maxlen = maxlen>(end-start+1)?maxlen:(end-start+1);
+        ++end;
+        while( 0 != map[ (int)*(s+end) ] )//将要加入的新元素与map内元素冲突
+        {
+            map[ (int)*(s+start) ] = 0;
+               ++start;
+        }
+        map[(int)*(s+end)] = 1;
+    }
+
+    return maxlen;
+}
 
 
 
